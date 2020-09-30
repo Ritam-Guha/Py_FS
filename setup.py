59420d0867a6f77d4ddd0f46c3c3d7d5b2d2a24f
Ritam Guha <ritz@Ritams-MacBook-Pro.local>
2020-09-30 13:39:39 +0530

    PyPI settings

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='PyFS',  
-     version='0.0.1',
+     name='Py_FS',  
+     version='0.0.0',
      author="Ritam Guha",
      author_email="ritamguha16@gmail.com",
      description="A Python Package for Feature Selection",
      long_description=long_description,
     long_description_content_type="text/markdown",
-     url="https://github.com/Ritam-Guha/PyFS",
+     url="https://github.com/Ritam-Guha/Py_FS",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
