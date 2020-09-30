c137561115da56ebb1205db601d3d5cb0111d097
Ritam Guha <ritz@Ritams-MacBook-Pro.local>
2020-09-30 12:40:35 +0530

    uploaded setuptools

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='PyFS',  
+     version='0.0.1',
+     author="Ritam Guha",
+     author_email="ritamguha16@gmail.com",
+     description="A Python Package for Feature Selection",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
