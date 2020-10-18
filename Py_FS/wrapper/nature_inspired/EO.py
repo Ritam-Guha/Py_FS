332e49e1fd232f6c6266010da19552c87af1ea53
Ritam Guha <ritz@Ritams-MacBook-Pro.local>
2020-10-18 23:18:25 +0530

    minor changes

--- Py_FS/wrapper/nature_inspired/EO.py
+++ Py_FS/wrapper/nature_inspired/EO.py
@@ -1,10 +1,10 @@
 """
 
 Programmer: Ritam Guha
-Date of Development: 9/10/2020
+Date of Development: 18/10/2020
 This code has been developed according to the procedures mentioned in the following research article:
-"Mirjalili, S., Mirjalili, S. M., & Lewis, A. (2014). Grey wolf optimizer. 
-Advances in engineering software, 69, 46-61."
+"Faramarzi, A., Heidarinejad, M., Stephens, B., & Mirjalili, S. (2020). Equilibrium optimizer: A novel optimization algorithm. 
+Knowledge-Based Systems, 191, 105190."
 
 """
 
@@ -78,28 +78,26 @@ def EO(num_agents, max_iter, train_data, train_label, obj_function=compute_accur
 
     # pool initialization
     eq_pool = np.zeros((pool_size+1, num_features))
-    eq_fitness = np.zeros((1, pool_size))
-    eq_fitness[0, :] = 100
+    eq_fitness = np.zeros(pool_size)
+    eq_fitness[:] = float("-inf")
 
     for iter_no in range(max_iter):
         print('\n================================================================================')
         print('                          Iteration - {}'.format(iter_no+1))
-        print('================================================================================\n')
-
-        pop_new = np.zeros((num_agents, num_features))        
+        print('================================================================================\n')     
 
         # replacements in the pool
         for i in range(num_agents):
             for j in range(pool_size):                 
-                if fitness[0, i] <= eq_fitness[0, j]:
-                    eq_fitness[0, j] = fitness[0, i].copy()
+                if fitness[i] <= eq_fitness[j]:
+                    eq_fitness[j] = fitness[i].copy()
                     eq_pool[j, :] = particles[i, :].copy()
                     break
         
 
         best_particle = eq_pool[0,:]
                 
-        Cave = avg_concentration(eq_pool,pool_size,num_features)
+        Cave = avg_concentration(eq_pool, pool_size, num_features)
         eq_pool[pool_size] = Cave.copy()
 
         t = (1 - (iter_no/max_iter)) ** (a2*iter_no/max_iter)
@@ -107,28 +105,22 @@ def EO(num_agents, max_iter, train_data, train_label, obj_function=compute_accur
         for i in range(num_agents):
             
             # randomly choose one candidate from the equillibrium pool
-            random.seed(time.time() + 100 + 0.02*i)
-            inx = random.randint(0,pool_size)
+            inx = np.random.randint(0,pool_size)
             Ceq = np.array(eq_pool[inx])
 
             lambda_vec = np.zeros(np.shape(Ceq))
             r_vec = np.zeros(np.shape(Ceq))
             for j in range(num_features):
-                random.seed(time.time() + 1.1)
-                lambda_vec[j] = random.random()
-                random.seed(time.time() + 10.01)
-                r_vec[j] = random.random()
+                lambda_vec[j] = np.random.random()
+                r_vec[j] = np.random.random()
 
             F_vec = np.zeros(np.shape(Ceq))
             for j in range(num_features):
                 x = -1*lambda_vec[j]*t 
-                x = math.exp(x) - 1
+                x = np.exp(x) - 1
                 x = a1 * sign_func(r_vec[j] - 0.5) * x
 
-            random.seed(time.time() + 200)
-            r1 = random.random()
-            random.seed(time.time() + 20)
-            r2 = random.random()
+            r1, r2 = np.random.random(2)
             if r2 < GP:
                 GCP = 0
             else:
@@ -144,23 +136,18 @@ def EO(num_agents, max_iter, train_data, train_label, obj_function=compute_accur
                 temp = Ceq[j] + (particles[i][j] - Ceq[j])*F_vec[j] + G[j]*(1 - F_vec[j])/lambda_vec[j]                
                 temp = trans_function(temp)                
                 if temp>np.random.random():
-                    pop_new[i][j] = 1 - particles[i][j]
+                    particles[i][j] = 1 - particles[i][j]
                 else:
-                    pop_new[i][j] = particles[i][j]          
+                    particles[i][j] = particles[i][j]          
 
         # update final information
-        particles, fitness = sort_agents(pop_new, obj_function, data)
+        particles, fitness = sort_agents(particles, obj_function, data)
         display(particles, fitness, agent_name)
         
         # update Leader (best agent)
         if fitness[0] > Leader_fitness:
             Leader_agent = particles[0].copy()
             Leader_fitness = fitness[0].copy()
-
-        if alpha_fit > Leader_fitness:
-            Leader_fitness = alpha_fit
-            Leader_agent = alpha.copy()
-
 
         convergence_curve['fitness'][iter_no] = Leader_fitness
         convergence_curve['feature_count'][iter_no] = int(np.sum(Leader_agent))
@@ -200,6 +187,19 @@ def EO(num_agents, max_iter, train_data, train_label, obj_function=compute_accur
 
     return solution
 
+
+def avg_concentration(eq_pool, pool_size, dimension): 
+    # function to compute average concentration of the equilibrium pool   
+    avg = np.sum(eq_pool[0:pool_size,:], axis=0)         
+    avg = avg/pool_size
+    return avg
+
+
+def sign_func(x): 
+    # signum function
+    if x<0:
+        return -1
+    return 1
 
 if __name__ == '__main__':
 
